76f8a18b6f6a936161923333f5ca21b45623a561
NickSramcik <Nick.Sramcik@gmail.com>
2022-09-25 20:55:33 -0700

    tailwind adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    ".views.{html,js}",
+    "./public/**/*. {html,js}",
+    // "./node_modules/tw-elements/dist/js/**/*.min.js"
+  ],
   theme: {
     extend: {},
   },
@@ -8,4 +12,5 @@ module.exports = {
   daisyui: {
     themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
   },
+  // plugins: [require('daisyui'), require('tw-elements/dist/plugin')],
 }
