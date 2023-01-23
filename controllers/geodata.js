dd2fea37d52ea2c1704cadc0b8694d1df30c39b3
NickSramcik <Nick.Sramcik@gmail.com>
2023-01-23 13:56:10 -0800

    Fire Cause Cleanup

--- controllers/geodata.js
+++ controllers/geodata.js
@@ -86,6 +86,10 @@ async function addFire(firePoint) {
                 if (!newFireCause) newFireCause = 'Perscribed';
                 newName = newName.split(' ').map(e => e == 'Rx' || e == Pb ? 'Perscribed Burn' : e).join(' ');
         };
+        if (newName.includes('Piles')) {
+            newFireCause = 'Perscribed';
+        };
+
         // Fix discovery date
         let newDate = (new Date(firePoint.properties.FireDiscoveryDateTime)).toString();
 
