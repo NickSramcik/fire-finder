a811426cc45a2a38becade695d6e31bc200843ab
NickSramcik <Nick.Sramcik@gmail.com>
2023-03-27 15:04:03 -0700

    geodata controller refactoring

--- controllers/geodata.js
+++ controllers/geodata.js
@@ -1,18 +1,17 @@
-// This controller handles automatic fire perimeter & point data collection and processing 
+// This controller handles automatic fire perimeter & point data collection and processing
 
-const GeoJson = require("../models/GeoJson");
-const Fire = require("../models/Fire");
-
+const GeoJson = require('../models/GeoJson');
+const Fire = require('../models/Fire');
 
 async function deletePerimeter(geoJsonName) {
     try {
         // Delete geojson from database
         await GeoJson.deleteMany({ dataName: geoJsonName });
-        console.log("Deleted Perimeter GeoJson");
+        console.log('Deleted Perimeter GeoJson');
     } catch (err) {
         console.log(err);
-    };
-};
+    }
+}
 
 async function uploadPerimeter(geojson) {
     try {
@@ -20,29 +19,30 @@ async function uploadPerimeter(geojson) {
         await deletePerimeter('autoPerimeter');
         // Upload new Fire Perimeter data to database
         await GeoJson.create({
-          dataName: 'autoPerimeter',
-          geoJsonData: geojson,
-          // user: req.user.id,
-          // userName: req.user.userName,
+            dataName: 'autoPerimeter',
+            geoJsonData: geojson,
+            // user: req.user.id,
+            // userName: req.user.userName,
         });
-      console.log("Fire Perimeter GeoJson data has been added!");
+        console.log('Fire Perimeter GeoJson data has been added!');
     } catch (err) {
         console.log(err);
-    };
-};
+    }
+}
 
 async function fetchPerimeterData() {
     // Fire Perimeter Data from NIFC
     // Visit https://nifc.maps.arcgis.com/home/item.html?id=d1c32af3212341869b3c810f1a215824
-    const perimeterUrl = 'https://services3.arcgis.com/T4QMspbfLg3qTGWY/ArcGIS/rest/services/WFIGS_Interagency_Perimeters_Current/FeatureServer/0/query?where=1%3D1&outFields=poly_IncidentName&f=pgeojson&token='
-    
+    const perimeterUrl =
+        'https://services3.arcgis.com/T4QMspbfLg3qTGWY/ArcGIS/rest/services/WFIGS_Interagency_Perimeters_Current/FeatureServer/0/query?where=1%3D1&outFields=poly_IncidentName&f=pgeojson&token=';
+
     try {
         const geodata = await (await fetch(perimeterUrl)).json();
         await uploadPerimeter(geodata);
     } catch (err) {
         console.log(err);
-    };
-};
+    }
+}
 
 async function deletePoints(user) {
     try {
@@ -51,112 +51,109 @@ async function deletePoints(user) {
         console.log(`Deleted Fire Points created by ${user}`);
     } catch (err) {
         console.log(err);
-    };
-};
+    }
+}
 
 async function addFire(firePoint) {
     try {
-        let totalBehavior = 
-        `${firePoint.properties.FireBehaviorGeneral},${firePoint.properties.FireBehaviorGeneral1},${firePoint.properties.FireBehaviorGeneral2},${firePoint.properties.FireBehaviorGeneral3}`;
+        let totalBehavior = `${firePoint.properties.FireBehaviorGeneral},${firePoint.properties.FireBehaviorGeneral1},${firePoint.properties.FireBehaviorGeneral2},${firePoint.properties.FireBehaviorGeneral3}`;
         // Clean up fire behavior data, remove nulls & duplicates
-        totalBehavior = totalBehavior.split(',').filter((e, i, a) => e != 'null' && i == a.indexOf(e)).join(', ');
+        totalBehavior = totalBehavior
+            .split(',')
+            .filter((e, i, a) => e != 'null' && i == a.indexOf(e))
+            .join(', ');
         // If no fire behavior data is present, give it value 'unknown'
         if (!totalBehavior) totalBehavior = 'Unknown';
-        
-        // Take the complex name if it exists, otherwise use incident name
-        let newName = firePoint.properties.IncidentName;
-        // Standardize capitalization and trim empty spaces
-        newName = newName.trim().toLowerCase().split(' ').filter(e => e)
-                         .map(e => e[0].toUpperCase() + e.slice(1)).join(' ');
-        // Add fire to end of name if it's not already there
-        if (!newName.includes('Fire') && 
-            !newName.includes('Rx') && 
-            !newName.includes('Pb') && 
-            !newName.includes('Prep') && 
-            !newName.includes('Piles') && 
-            !newName.includes('Tree Removal') && 
-            !newName.includes('Complex')) {
-                newName += ' Fire'
-        };
-        let newFireCause = firePoint.properties.FireCause;
 
-        // Change Pb & Rx to Prescribed Burn
-        if (newName.includes('Pb') ||
-            newName.includes('Rx')) {
-                if (!newFireCause) newFireCause = 'Prescribed';
-                newName = newName.split(' ').map(e => e == 'Rx' || e == 'Pb' ? 'Prescribed Burn' : e).join(' ');
-        };
-        if (newName.includes('Piles')) {
-            newFireCause = 'Prescribed';
-        };
-        // Change Bp to Burn Piles
-        if (newName.includes('Bp')) {
-            newFireCause = 'Prescribed';
-            newName = newName.split(' ').map(e => e == 'Bp' ? 'Burn Piles' : e).join(' ');
+        // Organize fire names. Add fire if there is no title. Standarize capitalization.
+        let newName = firePoint.properties.IncidentName.trim()
+            .toLowerCase()
+            .replace(/\b\w/g, c => c.toUpperCase());
+
+        if (!/(Fire|Rx|Pb|Prep|Piles|Tree Removal|Complex)\b/.test(newName)) {
+            newName += ' Fire';
         }
 
-        // TODO: .includes() method is inefficient, replace with better algorithm 
+        let newFireCause = firePoint.properties.FireCause;
+
+        // Check for prescribed burns, fix jargon and set fire cause
+        if (/(Rx|Bp|Pb|Prep|Piles)\b/.test(newName)) {
+            newFireCause = 'Prescribed';
+
+            newName = newName.replace(/Pb|Rx|Bp/g, match => {
+                switch (match) {
+                    case 'Pb':
+                        return 'Prescribed Burn';
+                    case 'Rx':
+                        return 'Prescribed Burn';
+                    case 'Bp':
+                        return 'Burn Piles';
+                }
+            });
+        }
 
         // Fix discovery date
-        let newDate = (new Date(firePoint.properties.FireDiscoveryDateTime)).toString();
+        let newDate = new Date(
+            firePoint.properties.FireDiscoveryDateTime
+        ).toString();
 
         // Add each point to the database as a fire object
         await Fire.create({
-          fireName: newName,
-          latitude: firePoint.geometry.coordinates[1],
-          longitude: firePoint.geometry.coordinates[0],
-          fireSize: firePoint.properties.IncidentSize,
-          fireBehavior: totalBehavior,
-          fireCause: newFireCause,
-          discoveryDate: newDate,
-          percentContained: firePoint.properties.PercentContained,
-          userName: 'system-auto'
-        })
-    }catch (err) {
+            fireName: newName,
+            latitude: firePoint.geometry.coordinates[1],
+            longitude: firePoint.geometry.coordinates[0],
+            fireSize: firePoint.properties.IncidentSize,
+            fireBehavior: totalBehavior,
+            fireCause: newFireCause,
+            discoveryDate: newDate,
+            percentContained: firePoint.properties.PercentContained,
+            userName: 'system-auto',
+        });
+    } catch (err) {
         console.log(err);
-    };
+    }
 }
 
 async function fetchPointData() {
     // Fire Point Data from NIFC
     // Visit https://nifc.maps.arcgis.com/home/item.html?id=4181a117dc9e43db8598533e29972015
-    const firePointUrl = 'https://services3.arcgis.com/T4QMspbfLg3qTGWY/ArcGIS/rest/services/WFIGS_Incident_Locations_Current/FeatureServer/0/query?where=1%3D1&outFields=FinalAcres%2CIncidentName%2CPercentContained%2CIncidentSize%2CFireBehaviorGeneral%2CFireCause%2CFireDiscoveryDateTime%2CFireBehaviorGeneral3%2CFireBehaviorGeneral2%2CFireBehaviorGeneral1&f=pgeojson&token=';
-
+    const firePointUrl =
+        'https://services3.arcgis.com/T4QMspbfLg3qTGWY/ArcGIS/rest/services/WFIGS_Incident_Locations_Current/FeatureServer/0/query?where=1%3D1&outFields=FinalAcres%2CIncidentName%2CPercentContained%2CIncidentSize%2CFireBehaviorGeneral%2CFireCause%2CFireDiscoveryDateTime%2CFireBehaviorGeneral3%2CFireBehaviorGeneral2%2CFireBehaviorGeneral1&f=pgeojson&token=';
 
     try {
         const fireData = await (await fetch(firePointUrl)).json();
         console.log(fireData);
-        let fireCount = 0;    
+        let fireCount = 0;
         for (point of fireData.features) {
             // console.log(`Adding ${point.attributes.IncidentName}`)
             await addFire(point);
             fireCount++;
-        };
-        
-        console.log(`Added ${fireCount} Fire Points`)
+        }
+
+        console.log(`Added ${fireCount} Fire Points`);
     } catch (err) {
         console.log(err);
-    };
-};
+    }
+}
 
 async function getGeoData() {
     try {
         await deletePoints('system-auto');
         await fetchPerimeterData();
         await fetchPointData();
-    }catch (err) {
+    } catch (err) {
         console.log(err);
     }
 }
 
 async function refreshGeoJson(req, res) {
-   try {
-    console.log('Refreshing GeoJson...');
-    const result = await getGeoData();
-    res.send(result);
-   }catch (err) {
-    console.log(err);
-   }
+    try {
+        console.log('Refreshing GeoJson...');
+        const result = await getGeoData();
+        res.send(result);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,65 +1,66 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const passport = require("passport");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const methodOverride = require("method-override");
-const flash = require("express-flash");
-const logger = require("morgan");
-const connectDB = require("./config/database");
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const methodOverride = require('method-override');
+const flash = require('express-flash');
+const logger = require('morgan');
+const connectDB = require('./config/database');
 const cron = require('node-cron');
 const dataScraper = require('./data-scraper.js');
-const mainRoutes = require("./routes/main");
-const fireRoutes = require("./routes/fires");
-
-
+const mainRoutes = require('./routes/main');
+const fireRoutes = require('./routes/fires');
 
 //Use .env file in config folder
-require("dotenv").config({ path: "./config/.env" });
+require('dotenv').config({ path: './config/.env' });
 
 // Passport config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 // Connect asyncronously for Cyclic's serverless architecture
 const { MongoClient } = require('mongodb');
 const uri = process.env.DB_STRING;
-const client = new MongoClient(uri, {useUnifiedTopology: true});
+const client = new MongoClient(uri, { useUnifiedTopology: true });
 const dbS = connectDB().then(() => {
-  client.connect(async err => {
-    //Connect To Database
-    if(err){ console.error(err); return false;}
-    // connection to mongo is successful, listen for requests
-    app.listen(3000, () => {
-      console.log("Server is running, you better catch it!");
-    })
-  });
-})
+    client.connect(async err => {
+        //Connect To Database
+        if (err) {
+            console.error(err);
+            return false;
+        }
+        // connection to mongo is successful, listen for requests
+        app.listen(3000, () => {
+            console.log('Server is running, you better catch it!');
+        });
+    });
+});
 
 //Using EJS for views
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 //Static Folder
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 //Body Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Logging
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 //Use forms for put / delete
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 
 // Setup Sessions - stored in MongoDB
 app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  })
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
 );
 
 // Passport middleware
@@ -70,8 +71,8 @@ app.use(passport.session());
 app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
-app.use("/", mainRoutes);
-app.use("/fire", fireRoutes);
+app.use('/', mainRoutes);
+app.use('/fire', fireRoutes);
 
 //Tailwind
 app.use(express.static('node_modules/tw-elements/dist/js'));
