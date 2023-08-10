be0840e39e43598a3b1871554a1a226f04afa978
NickSramcik <Nick.Sramcik@gmail.com>
2023-08-10 12:32:24 -0700

    debugging

--- public/js/index.js
+++ public/js/index.js
@@ -44,7 +44,7 @@ function initMap() {
         zoom: 5,
         center: { lat: 37.7749, lng: -122.4194 },
     };
-    // Add Current Location
+    // Add Current Locationpu
     let locationIcon = {
       path: 'M12 0c-5.522 0-10 4.395-10 9.815 0 5.505 4.375 9.268 10 14.185 5.625-4.917 10-8.68 10-14.185 0-5.42-4.478-9.815-10-9.815zm1 17.932v-3.932h-2v3.932c-3.618-.453-6.479-3.315-6.931-6.932h3.931v-2h-3.931c.452-3.617 3.314-6.479 6.931-6.931v3.931h2v-3.931c3.617.452 6.479 3.314 6.931 6.931h-3.931v2h3.931c-.452 3.617-3.314 6.479-6.931 6.932z',
       fillColor: 'deepskyblue',
@@ -77,6 +77,7 @@ function initMap() {
     }
 
     function renderGeoJson(geojson) {
+        console.log('geojson data: ', geojson)
         // Style geojson data
         map.data.setStyle({
             // icon: '//example.com/path/to/image.png',
